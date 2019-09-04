b92c38c1485e4404bc083792cb9d0b5bcf40748c
konghouyin <1360234119@qq.com>
2019-09-04 21:45:53 +0800

    音频过滤完成

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 const {app,ipcMain,dialog} = require('electron')
-//const musicLoader = require('./tool/musicLoader')
+const fs = require('fs')
+const {MusicLoader} = require('./tool/musicLoader')
 const {Storage} = require('./tool/storage')
 const {addWindow} = require('./tool/addWindow')
 
 let storage = new Storage()
-
+let musicLoader = new MusicLoader()
 
 app.on('ready', function() {
 	new addWindow({
@@ -14,7 +15,16 @@ app.on('ready', function() {
 })
 
 ipcMain.on('addMusic', async function() {
-	console.log(await dialog.showOpenDialog({title:"请选择文件夹",properties: ['openDirectory'] }))
+	directory = await dialog.showOpenDialog({title:"请选择文件夹",properties: ['openDirectory'] })
+	fs.readdir(directory.filePaths[0],(err,files)=>{
+		if(err) throw err
+		let reg = /.(mp3|m4a|acc|flac)$/i
+		console.log(files.filter(function(item){
+			if(reg.test(item)){
+				return item
+			}
+		}))
+	})
 	new addWindow({
 		minWidth: 500,
 		minHeight: 400,
--- tool/musicLoader.js
+++ tool/musicLoader.js
@@ -1,45 +1,55 @@
-const audioLoad = require('id3js')
-console.log(audioLoad.fromPath)
-audioLoad.fromPath('D:\\音乐\\A New Day_Mark Petrie.mp3').then(function (res) {
-	function _arrayBufferToBase64( raw ) {
-	     var base64 = '';
-	     var encodings = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
-	     var bytes = new Uint8Array(raw);
-	     var byteLength = bytes.byteLength;
-	     var byteRemainder = byteLength % 3;
-	     var mainLength = byteLength - byteRemainder;
-	     var a, b, c, d;
-	     var chunk;  
-	     // Main loop deals with bytes in chunks of 3
-	     for (var i = 0; i < mainLength; i = i + 3) {
-	      // Combine the three bytes into a single integer
-	      chunk = (bytes[i] << 16) | (bytes[i + 1] << 8) | bytes[i + 2];
-	      // Use bitmasks to extract 6-bit segments from the triplet
-	      a = (chunk & 16515072) >> 18; // 16515072 = (2^6 - 1) << 18
-	      b = (chunk & 258048) >> 12; // 258048 = (2^6 - 1) << 12
-	      c = (chunk & 4032) >> 6; // 4032 = (2^6 - 1) << 6
-	      d = chunk & 63; // 63 = 2^6 - 1
-	      // Convert the raw binary segments to the appropriate ASCII encoding
-	      base64 += encodings[a] + encodings[b] + encodings[c] + encodings[d];
-	     }
-	     // Deal with the remaining bytes and padding
-	     if (byteRemainder == 1) {
-	      chunk = bytes[mainLength];
-	      a = (chunk & 252) >> 2 // 252 = (2^6 - 1) << 2;
-	      // Set the 4 least significant bits to zero
-	      b = (chunk & 3) << 4 // 3 = 2^2 - 1;
-	      base64 += encodings[a] + encodings[b] + '==';
-	     }
-	     else if (byteRemainder == 2) {
-	      chunk = (bytes[mainLength] << 8) | bytes[mainLength + 1];
-	      a = (chunk & 16128) >> 8 // 16128 = (2^6 - 1) << 8;
-	      b = (chunk & 1008) >> 4 // 1008 = (2^6 - 1) << 4;
-	      // Set the 2 least significant bits to zero
-	      c = (chunk & 15) << 2 // 15 = 2^4 - 1;
-	      base64 += encodings[a] + encodings[b] + encodings[c] + '=';
-	     }
-	     return "data:image/jpeg;base64," + base64;
-	  }
-  console.log(_arrayBufferToBase64(res.images[0].data)) // => <AudioBuffer>
-  document.getElementById('image').setAttribute('src',_arrayBufferToBase64(res.images[0].data))
-})
+const load = require('id3js')
+class audioLoad {
+	constructor(arg) {
+
+	}
+
+	check(path) {
+		load.fromPath(path).then(function(res) {
+			return (audioLoad.arrayBufferToBase64(res.images[0].data)) // => <AudioBuffer>
+		})
+	}
+
+	static arrayBufferToBase64() {
+		var base64 = '';
+		var encodings = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
+		var bytes = new Uint8Array(raw);
+		var byteLength = bytes.byteLength;
+		var byteRemainder = byteLength % 3;
+		var mainLength = byteLength - byteRemainder;
+		var a, b, c, d;
+		var chunk;
+		// Main loop deals with bytes in chunks of 3
+		for (var i = 0; i < mainLength; i = i + 3) {
+			// Combine the three bytes into a single integer
+			chunk = (bytes[i] << 16) | (bytes[i + 1] << 8) | bytes[i + 2];
+			// Use bitmasks to extract 6-bit segments from the triplet
+			a = (chunk & 16515072) >> 18; // 16515072 = (2^6 - 1) << 18
+			b = (chunk & 258048) >> 12; // 258048 = (2^6 - 1) << 12
+			c = (chunk & 4032) >> 6; // 4032 = (2^6 - 1) << 6
+			d = chunk & 63; // 63 = 2^6 - 1
+			// Convert the raw binary segments to the appropriate ASCII encoding
+			base64 += encodings[a] + encodings[b] + encodings[c] + encodings[d];
+		}
+		// Deal with the remaining bytes and padding
+		if (byteRemainder == 1) {
+			chunk = bytes[mainLength];
+			a = (chunk & 252) >> 2 // 252 = (2^6 - 1) << 2;
+			// Set the 4 least significant bits to zero
+			b = (chunk & 3) << 4 // 3 = 2^2 - 1;
+			base64 += encodings[a] + encodings[b] + '==';
+		} else if (byteRemainder == 2) {
+			chunk = (bytes[mainLength] << 8) | bytes[mainLength + 1];
+			a = (chunk & 16128) >> 8 // 16128 = (2^6 - 1) << 8;
+			b = (chunk & 1008) >> 4 // 1008 = (2^6 - 1) << 4;
+			// Set the 2 least significant bits to zero
+			c = (chunk & 15) << 2 // 15 = 2^4 - 1;
+			base64 += encodings[a] + encodings[b] + encodings[c] + '=';
+		}
+		return "data:image/jpeg;base64," + base64;
+	}
+}
+module.exports = {
+	MusicLoader: audioLoad
+}
+//支持的音频格式有:MP3,aac,flac,m4a
