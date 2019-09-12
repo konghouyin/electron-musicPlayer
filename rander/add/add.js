7352e5e0e4af8609a6ce7cf9f8520b12a4c2f2a3
konghouyin <1360234119@qq.com>
2019-09-12 17:56:21 +0800

    引入新字体

--- rander/add/add.js
+++ rander/add/add.js
@@ -10,9 +10,37 @@ var app = new Vue({
 	},
 	mounted() {
 		ipcRenderer.send('getMusicAnalysis');
-		ipcRenderer.on('TouchMusicAnalysis', (event,msg) => {
+		ipcRenderer.on('TouchMusicAnalysis', (event, msg) => {
 			this.musicList = msg
 		})
-	}
+	},
+	methods: {
+		showImage(msg) {
+			if (msg == null) {
+				return ""
+			} else if (msg.image == undefined) {
+				return ""
+			} else {
+				return msg.image
+			}
 
+		},
+		itemName(name) {
+			return name.substring(0, name.lastIndexOf('.'))
+		},
+		showAlbum(msg) {
+			if (msg == null) {
+				return ""
+			} else {
+				return msg.album
+			}
+		},
+		showArtist(msg) {
+			if (msg == null) {
+				return ""
+			} else {
+				return msg.artist
+			}
+		}
+	}
 })
